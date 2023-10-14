cac98b531004c002b46671f7cee4d68509d242de
Chanhhhhhh <letrongfls2003@gmail.com>
2023-10-14 19:22:01 +0700

    add lab1

--- lab01/HelloWord.java
+++ lab01/HelloWord.java
@@ -1,7 +1,7 @@
-package lab01;
-
+// Text-printing program
 public class HelloWord {
     public static void main(String[] args) {
+        System.out.println("Le Ba Trong - 20215153");
         System.out.println("Xin chao \n cac ban!");
         System.out.println("Hello \t world!");
     }
