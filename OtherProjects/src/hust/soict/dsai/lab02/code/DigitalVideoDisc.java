f783e0f9cab944f1678ec1d36f42d5f192f307f3
Chanhhhh <letrongfls2003@gmail.com>
2023-11-14 15:12:45 +0700

    add lab3

---
+++ OtherProjects/src/hust/soict/dsai/lab02/code/DigitalVideoDisc.java
@@ -0,0 +1,56 @@
+package hust.soict.dsai.lab02.code;
+
+public class DigitalVideoDisc {
+    // Lê Bá Trọng - 20215153
+    private  String title;
+    private  String category;
+    private String director;
+    private  int length;
+    private  float cost;
+    // constructor
+    public DigitalVideoDisc(String title) {
+        this.title = title;
+    }
+    // constructor
+    public DigitalVideoDisc(String title, String category, float cost) {
+        this.title = title;
+        this.category = category;
+        this.cost = cost;
+    }
+    // constructor
+    public DigitalVideoDisc(String title, String category, float cost, String director) {
+        this.title = title;
+        this.category = category;
+        this.cost = cost;
+        this.director = director;
+    }
+
+    // constructor
+    public DigitalVideoDisc(String title, String category, String director, int length, float cost) {
+        this.title = title;
+        this.category = category;
+        this.cost = cost;
+        this.director = director;
+        this.length = length;
+    }
+
+
+    // getter
+    public String getTitle() {
+        return title;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public String getDirector() {
+        return director;
+    }
+
+    public int getLenght() { return length;}
+
+    public float getCost() {
+        return cost;
+    }
+}
--- src/lab01/HelloWord.java
+++
@@ -1,8 +0,0 @@
-// Text-printing program
-public class HelloWord {
-    public static void main(String[] args) {
-        System.out.println("Le Ba Trong - 20215153");
-        System.out.println("Xin chao \n cac ban!");
-        System.out.println("Hello \t world!");
-    }
-}
